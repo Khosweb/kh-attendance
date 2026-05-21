a4090da9cc757df2fd2950f197d890cc737c27ac
Khos <somsak.taree@gmail.com>
2026-05-21 14:49:27 +0700

    edit /me to /auth/me

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -28,7 +28,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
       
       if (token) {
         try {
-          const response = await fetch(`${API_URL}/me`, {
+          const response = await fetch(`${API_URL}/auth/me`, {
             headers: {
               'Authorization': `Bearer ${token}`
             }
